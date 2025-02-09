24522f635893e7cb63ec6ef885d916080661219f
Joel S <jmsmith.coding@gmail.com>
2025-02-09 15:17:49 -0800

    "inital commit, project structured nicely and consistently with group, will create new branch, will try and get the page to render for group to work on back end."

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,49 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App";
+import Home from "./pages/Home.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import Genres from "./pages/Genres.tsx";
+import MyReelRanks from "./pages/MyReelRanks.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement:<ErrorPage/>,
+    children: [
+      {
+        path: "/About",
+        element: <About/>,
+      },
+      {
+        path: "/Genres",
+        element: <Genres />,
+      },
+      {
+        path: "/HomePage",
+        element: <Home />,
+      },
+      {
+        path: "/HomePage",
+        element: <HomePage/>,
+      },
+      {
+        path: "/MyReelRanks",
+        element: <MyReelRanks/>,
+      },
+      {
+        path: "/UserProfile",
+        element: <UserProfile/>,
+      }
+    ],
+  },
+]);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/Genres.tsx
@@ -0,0 +1,11 @@
+import Genre from "../components/Genre";
+
+const Genres = () => {
+  return (
+    <div className="genres-page">
+      <Genre />
+    </div>
+  );
+};
+
+export default Genres;
